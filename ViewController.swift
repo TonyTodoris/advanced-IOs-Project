72c51266e84c68454f6c3613c5650e00bd31843f
Antonis <antonistodoris@gmail.com>
2019-01-25 11:52:15 +0100

    Managed to parse Json data and make table view show each user in the cells

--- ViewController.swift
+++ ViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Canquery Déborah on 24/01/2019.
 //  Copyright © 2019 Epita. All rights reserved.
 //
-
+import Foundation
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
@@ -24,7 +24,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return list.count
+        return users.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -36,9 +36,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
              cell.backgroundColor = UIColor.init(red: 180.0/255.0, green: 126.0/255.0, blue: 179.0/255.0, alpha: 0.1)
         }
         
-        // cell.load(users[indexPath.item])
-        let user = User(Title: "Title", First: "Antonios", Last: "Todoris", Street: "Ververoda", City: "Kranidi", Email: "anto@gmail.com", Phone: "1234", Cell: "5678", LargePictureURL: "https://randomuser.me/api/portraits/thumb/men/65.jpg", smallPictureURL: "https://randomuser.me/api/portraits/thumb/men/65.jpg")
-        cell.load(user)
+        cell.load(users[indexPath.item])
+       /* let user = User(Title: "Title", First: "Antonios", Last: "Todoris", Street: "Ververoda", City: "Kranidi", Email: "anto@gmail.com", Phone: "1234", Cell: "5678", LargePictureURL: "https://randomuser.me/api/portraits/thumb/men/65.jpg", smallPictureURL: "https://randomuser.me/api/portraits/thumb/men/65.jpg")
+        cell.load(user) */
         return cell
     }
     
@@ -48,26 +48,88 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     
     func getContent(){
-        
-            guard let url = URL(string: "https://randomuser.me/api/?results=25&seed=todoris") else {return}
-            URLSession.shared.dataTask(with: url) { (data, response, error) in
-                
-            guard let data = data else { return }
-                
-           /* let dataAsString = String(data: data, encoding: .utf8)
-            print(dataAsString)*/
-            
-            
-            do{
-             
-            } catch let jsonErr{
-                print("error serializing json", jsonErr)
+        let url = URL(string: "https://randomuser.me/api/?results=25&seed=todoris")
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            if (error != nil){
+                print("ERROR")
             }
-                
+            else {
+                if let content = data
+                {
+                    do{
+                        //Array
+                        let myJson = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        
+                        if let results = myJson["results"] as? NSArray
+                        {
+                            for result in results{
+                                if let dictRes = result as? NSDictionary
+                                {
+                                    let cell = (dictRes["cell"] as? String)!
+                                    let phone = (dictRes["phone"] as? String)!
+                                    let email = (dictRes["email"] as? String)!
+                                    
+                                    var title = ""
+                                    var first = ""
+                                    var last =  ""
+                                    
+                                    var street = ""
+                                    var city = ""
+                                    
+                                    var large = ""
+                                    var medium = ""
+                                    var thumbnail = ""
+                                    
+                                    if let dictName = dictRes["name"] as? NSDictionary //Creating dictionary for name
+                                    {
+                                        title = (dictName["title"] as? String)!
+                                        first = (dictName["first"] as? String)!
+                                        last =  (dictName["last"] as? String)!
+                                    }
+                                
+                                    if let dictLocation = dictRes["location"] as? NSDictionary // Creating dictionary for location
+                                    {
+                                        street = (dictLocation["street"] as? String)!
+                                        city = (dictLocation["city"] as? String)!
+                                    }
+                                    
+                                    if let dictPicture =  dictRes["picture"] as? NSDictionary
+                                    {
+                                        large = (dictPicture["large"] as? String)!
+                                        medium = (dictPicture["medium"] as? String)!
+                                        thumbnail = (dictPicture["thumbnail"] as? String)!
+                                    }
+                                    
+                                    let newUser = User(Title: title,
+                                                       First: first,
+                                                       Last: last,
+                                                       Street: street,
+                                                       City: city,
+                                                       Email: email,
+                                                       Phone: phone,
+                                                       Cell: cell,
+                                                       LargePictureURL: large,
+                                                       smallPictureURL: thumbnail)
+                                    self.users.append(newUser)
+                                    
+                                    DispatchQueue.main.async {
+                                        self.tableView.reloadData()
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    catch{
+                        
+                    }
+                }
+            }
             
-                
-            }.resume()
-                
+        }
+        task.resume()
+        
+   
+        
     }
 
 }
